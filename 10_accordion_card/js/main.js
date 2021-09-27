836aabd7895a42b8f773b007d3c905ad7cfef660
YevgS <onegin2003@mail.ru>
2021-09-27 20:02:14 +0300

    10_accordion

---
+++ 10_accordion_card/js/main.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".faq__title").forEach(item =>
+	item.addEventListener("click", () => {
+		const parent = item.parentNode;
+
+		if (parent.classList.contains("active")) {
+			parent.classList.remove("active");
+		} else {
+			document.querySelectorAll(".faq__item").forEach(elem => elem.classList.remove("active"));
+			parent.classList.add("active");
+		}
+	})
+);
